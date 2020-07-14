c678d6dd134476b24c92967009d69468ad8a837d
Ozzy <cfhamlet@gmail.com>
2020-07-14 20:59:08 +0800

    fix: isort 5.0.0

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -13,4 +13,4 @@ FILES="os_scrapy_rq_crawler tests setup.py"
 
 ${PREFIX}autoflake --in-place --recursive --remove-all-unused-imports --remove-unused-variables ${FILES}
 ${PREFIX}black --exclude=".pyi$" ${FILES}
-${PREFIX}isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --combine-as --line-width 88 --recursive --apply ${FILES}
+${PREFIX}isort --multi-line=3 --trailing-comma --force-grid-wrap=0 --combine-as --line-width 88 ${FILES}
