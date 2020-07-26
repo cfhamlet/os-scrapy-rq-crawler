cb54f3b1ac331b9a8e4ea0541ea14e109a1457ef
Ozzy <cfhamlet@gmail.com>
2020-07-26 19:42:56 +0800

    add: redirect

--- os_scrapy_rq_crawler/middlewares.py
+++ os_scrapy_rq_crawler/middlewares.py
@@ -5,99 +5,48 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
-from scrapy import signals
+from scrapy.downloadermiddlewares.redirect import (
+    MetaRefreshMiddleware as MetaRfsMiddleware,
+    RedirectMiddleware as RdrMiddleware,
+)
+from scrapy.http import Request
+
+S_DOWNLOAD_DELAY = "download_delay"
+S_DOWNLOAD_DELAY_RDR_ROLLBACK = S_DOWNLOAD_DELAY + "_rdr_rollback"
 
 
-class ScrapyRqCrawlerSpiderMiddleware:
-    # Not all methods need to be defined. If a method is not defined,
-    # scrapy acts as if the spider middleware does not modify the
-    # passed objects.
-
-    @classmethod
-    def from_crawler(cls, crawler):
-        # This method is used by Scrapy to create your spiders.
-        s = cls()
-        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
-        return s
-
-    def process_spider_input(self, response, spider):
-        # Called for each response that goes through the spider
-        # middleware and into the spider.
-
-        # Should return None or raise an exception.
-        return None
-
-    def process_spider_output(self, response, result, spider):
-        # Called with the results returned from the Spider, after
-        # it has processed the response.
-
-        # Must return an iterable of Request, dict or Item objects.
-        for i in result:
-            yield i
-
-    def process_spider_exception(self, response, exception, spider):
-        # Called when a spider or process_spider_input() method
-        # (from other spider middleware) raises an exception.
-
-        # Should return either None or an iterable of Request, dict
-        # or Item objects.
-        pass
-
-    def process_start_requests(self, start_requests, spider):
-        # Called with the start requests of the spider, and works
-        # similarly to the process_spider_output() method, except
-        # that it doesn’t have a response associated.
-
-        # Must return only requests (not items).
-        for r in start_requests:
-            yield r
-
-    def spider_opened(self, spider):
-        spider.logger.info("Spider opened: %s" % spider.name)
+def process_response(request, response, spider):
+    if isinstance(response, Request):
+        if (
+            S_DOWNLOAD_DELAY in request.meta
+            and request.meta[S_DOWNLOAD_DELAY] is not None
+        ):
+            request.meta[S_DOWNLOAD_DELAY_RDR_ROLLBACK] = request.meta[S_DOWNLOAD_DELAY]
+        request.meta[S_DOWNLOAD_DELAY] = None
+    else:
+        meta = request.meta
+        if (
+            S_DOWNLOAD_DELAY in meta
+            and "redirect_urls" in meta
+            and meta[S_DOWNLOAD_DELAY] is None
+        ):
+            meta.pop(S_DOWNLOAD_DELAY)
+            if S_DOWNLOAD_DELAY_RDR in meta:
+                meta[S_DOWNLOAD_DELAY] = meta.pop(S_DOWNLOAD_DELAY_RDR_ROLLBACK)
+    return response
 
 
-class ScrapyRqCrawlerDownloaderMiddleware:
-    # Not all methods need to be defined. If a method is not defined,
-    # scrapy acts as if the downloader middleware does not modify the
-    # passed objects.
-
-    @classmethod
-    def from_crawler(cls, crawler):
-        # This method is used by Scrapy to create your spiders.
-        s = cls()
-        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
-        return s
-
-    def process_request(self, request, spider):
-        # Called for each request that goes through the downloader
-        # middleware.
-
-        # Must either:
-        # - return None: continue processing this request
-        # - or return a Response object
-        # - or return a Request object
-        # - or raise IgnoreRequest: process_exception() methods of
-        #   installed downloader middleware will be called
-        return None
-
+class RedirectMiddleware(RdrMiddleware):
     def process_response(self, request, response, spider):
-        # Called with the response returned from the downloader.
+        response = super(RedirectMiddleware, self).process_response(
+            request, response, spider
+        )
+        return process_response(request, response, spider)
 
-        # Must either;
-        # - return a Response object
-        # - return a Request object
-        # - or raise IgnoreRequest
-        return response
 
-    def process_exception(self, request, exception, spider):
-        # Called when a download handler or a process_request()
-        # (from other downloader middleware) raises an exception.
-
-        # Must either:
-        # - return None: continue processing this exception
-        # - return a Response object: stops process_exception() chain
-        # - return a Request object: stops process_exception() chain
-        pass
-
-    def spider_opened(self, spider):
-        spider.logger.info("Spider opened: %s" % spider.name)
+class MetaRefreshMiddleware(MetaRfsMiddleware):
+    def process_response(self, request, response, spider):
+        response = super(MetaRefreshMiddleware, self).process_response(
+            request, response, spider
+        )
+        return process_response(request, response, spider)
